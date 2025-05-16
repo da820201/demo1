f4c199977ff73fe4b921364d3b86fa796381c21b
daniel.guan <daniel.guan@shell.fans>
2025-05-16 18:15:30 +0800

    update @@

--- schemes/user_schemes.py
+++ schemes/user_schemes.py
@@ -21,8 +21,7 @@ class ScapyStatus(Enum):
     Waiting = 2
 
 
-class Facebook(EmbeddedJsonModel):
-    dtsg: Optional[str] = None
+class FacebookGroup(EmbeddedJsonModel):
     uid: Optional[str] = Field(index=True, primary_key=True)
     name: Optional[str] = Field(index=True)
     scapy_status: Optional[int] = 0
@@ -32,7 +31,39 @@ class Facebook(EmbeddedJsonModel):
 
     class Meta:
         database = redis
-        global_key_prefix: str = "sf_account_facebook"
+        global_key_prefix: str = "sf_user_facebook_group"
+
+
+class FacebookPage(EmbeddedJsonModel):
+    uid: Optional[str] = Field(index=True, primary_key=True)
+    name: Optional[str] = Field(index=True)
+    profile_url: Optional[str] = None
+    head_pic_url: Optional[str] = None
+    followers: Optional[list[str]] = []
+    scapy_status: Optional[int] = 0
+    cookies_str: Optional[str] = None
+    create_time: Optional[float] = Field(default_factory=lambda: time.time())
+    update_time: Optional[float] = Field(default_factory=lambda: time.time())
+
+    class Meta:
+        database = redis
+        global_key_prefix: str = "sf_user_facebook_page"
+
+
+class Facebook(EmbeddedJsonModel):
+    dtsg: Optional[str] = None
+    uid: Optional[str] = Field(index=True, primary_key=True)
+    name: Optional[str] = Field(index=True)
+    profile_url: Optional[str] = None
+    pages: Optional[list[FacebookPage]] = None
+    groups: Optional[list[FacebookGroup]] = None
+    cookies_str: Optional[str] = None
+    create_time: Optional[float] = Field(default_factory=lambda: time.time())
+    update_time: Optional[float] = Field(default_factory=lambda: time.time())
+
+    class Meta:
+        database = redis
+        global_key_prefix: str = "sf_user_facebook"
 
 
 class Instagram(EmbeddedJsonModel):
@@ -65,21 +96,15 @@ class Threads(EmbeddedJsonModel):
         global_key_prefix: str = "sf_account_threads"
 
 
-class SocialMedias(EmbeddedJsonModel):
-    uid: str = Field(index=True, primary_key=True)
-    facebook: Optional[Facebook] = None
-    instagram: Optional[Instagram] = None
-    threads: Optional[Threads] = None
-
-    class Meta:
-        database = redis
-
-
 class UserAccount(JsonModel):
     uid: str = Field(index=True, primary_key=True)
     account: str = Field(index=True)
     password: str
-    social_medias: Optional[SocialMedias] = None
+    facebook: Optional[list[Facebook]] = None
+    instagram: Optional[list[Instagram]]  = None
+    threads: Optional[list[Threads]] = None
+    birthday: Optional[datetime.date]
+    phone_number: Optional[str]
     create_time: Optional[float] = Field(default_factory=lambda: time.time())
     update_time: Optional[float] = Field(default_factory=lambda: time.time())
 
