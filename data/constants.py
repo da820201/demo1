0cb056a5d12083245d590da2b93ec30df6a231d4
daniel.guan <daniel.guan@shell.fans>
2025-05-23 13:44:44 +0800

    update @@

---
+++ data/constants.py
@@ -0,0 +1,20 @@
+
+class Setting:
+    BLOK_VERSION_ID = "5f56efad68e1edec7801f630b5c122704ec5378adbee6609a448f105f34a9c73"
+    ANDROID_ID = ""
+    DEVICE_ID = ""
+
+
+class Path:
+    FacebookHost = "https://www.facebook.com"
+    ThreadsHost = "https://www.threads.com"
+    InstagramHost = "https://www.instagram.com"
+    InstagramGraphql = f"{InstagramHost}/graphql"
+    InstagramGraphqlQuery = f"{InstagramGraphql}/query"
+    InstagramAPIV1 = f"{InstagramHost}/api/v1"
+    ThreadsGraphql = f"{ThreadsHost}/graphql"
+    ThreadsGraphqlQuery = f"{ThreadsGraphql}/query"
+
+
+if __name__ == "__main__":
+    pass
